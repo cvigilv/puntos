b38da039485123fd63e6a49e3b3d0f26e2e46354
cvigilv <cvigil2@uc.cl>
2021-07-06 20:32:09 -0400

    feat: Add compatibility for multiple monitor setup
    Bought a new monitor for productivity reasons and decided to add some
    quality-of-life improvements to my window manager:
    
    - External monitor defaults to primary
    - 2 monitor layout will spawn a `polybar` for each monitor
    - Automatic detection of monitor layout for seamless integration of
      `polybar`

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,6 +5,11 @@ killall -q polybar
 
 # Launch bar
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+
 polybar cvv >>/tmp/polybar_cvv.log 2>&1 &
+if [[ $(xrandr --query | grep 'DisplayPort-0') = *connected* ]];
+then
+	polybar cvv_external >>/tmp/polybar_cvv.log 2>&1 &
+fi
 
 echo "Bars launched..."
