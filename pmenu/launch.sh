af0b9d5179957793c971e773fdd5bb5d2c1c242f
Carlos Vigil Vásquez <cvigil2@uc.cl>
2022-04-20 13:13:25 -0400

    feat(pmenu): bsp-layout toggle

--- pmenu/launch.sh
+++ pmenu/launch.sh
@@ -12,10 +12,11 @@ IMG:/home/carlos/documents/git/puntos/pmenu//src/play.png	play
 	IMG:/home/carlos/documents/git/puntos/pmenu/src/discord.png	WhatsApp	whatsapp-nativefier
 IMG:/home/carlos/documents/git/puntos/pmenu/src/settings.png	Settings
 	IMG:/home/carlos/documents/git/puntos/pmenu/src/keyboard.png	Keymap	/home/carlos/documents/git/puntos/pmenu/src/_toggle_keymap.sh
+	IMG:/home/carlos/documents/git/puntos/pmenu/src/shades.png	Eye-saver	redshift -x && redshift -o && notify-send -i /home/carlos/documents/git/puntos/pmenu/src/shades.png  "redshift" "Adjusted color temperature based in location"
 	IMG:/home/carlos/documents/git/puntos/pmenu/src/arandr.png	Screen-layout	arandr
 	IMG:/home/carlos/documents/git/puntos/pmenu/src/power_menu.png	Power menu
 		IMG:/home/carlos/documents/git/puntos/pmenu/src/reboot.png	Reboot	reboot
 		IMG:/home/carlos/documents/git/puntos/pmenu/src/shutdown.png	Shutdown	shutdown now
-		IMG:/home/carlos/documents/git/puntos/pmenu/src/sleep.png	Sleep	sleep
-	IMG:/home/carlos/documents/git/puntos/pmenu/src/shades.png	Eye-saver	redshift -x -o && notify-send -i /home/carlos/documents/git/puntos/pmenu/src/shades.png  "redshift" "Adjusted color temperature based in location"
+		IMG:/home/carlos/documents/git/puntos/pmenu/src/sleep.png	Sleep	systemctl suspend
+	IMG:/home/carlos/documents/git/puntos/pmenu/src/layout.png	Layout	bsp-layout cycle --layouts tall,rgrid,monocle && notify-send -i /home/carlos/documents/git/puntos/pmenu/src/layout.png  "bsp-layout" "Current layout: $(bsp-layout get 1)"
 EOF
